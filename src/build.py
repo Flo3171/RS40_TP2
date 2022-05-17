4c3179a5e4ebb799bb2521e209e43998ef6d326f
akieny <arnaudki@icloud.com>
2022-05-17 12:24:44 +0200

    Filled build.py (step 3).py

--- src/build.py
+++ src/build.py
@@ -18,7 +18,7 @@ SERVER_PRIVATE_KEY_FILENAME = RESOURCES_DIR + "server-private-key.pem"
 SERVER_CSR_FILENAME = RESOURCES_DIR + "server-csr.pem"
 SERVER_PUBLIC_KEY_FILENAME = RESOURCES_DIR + "server-public-key.pem"
 CA_PASSWORD = "Comment est votre blanquette ?"
-SERVER_PASSWORD = # A compléter
+SERVER_PASSWORD = "Elle est bonne."
 
 CA_CONFIGURATION = Configuration("FR", "Territoire de Belfort", "Sevenans", "UTBM_CA", "localhost") 
 SERVER_CONFIGURATION = Configuration("FR", "Territoire de Belfort", "Sevenans", "UTBM_SER", "localhost") 
@@ -28,13 +28,26 @@ certificate_authority = CertificateAuthority(CA_CONFIGURATION, CA_PASSWORD, CA_P
     # regardez en haut et ca/core.py
 
 # Création du server
-server =Server(#à compléter) 
+server =Server(SERVER_CONFIGURATION, SERVER_PASSWORD, SERVER_PRIVATE_KEY_FILENAME, SERVER_CSR_FILENAME)
     # regardez en haut et server/core.py
 
 # Signature du certificat par l'autorité de certification
-signed_certificate = # A compléter regardez ca/core.py et server/core.py
+signed_certificate = certificate_authority.sign(server.get_csr(), SERVER_PUBLIC_KEY_FILENAME)
+# A compléter regardez ca/core.py et server/core.py
 
 #impression des certificats à compléter regardez #print_pems
+print("CA Private Key:")
+print_pems.print_perms("ca-private-key.pem")
+print("CA Public Key:")
+print_pems.print_perms("ca-public-key.pem")
+print("Server Private Key:")
+print_pems.print_perms("server-private-key.pem")
+print("Server CRS:")
+print_pems.print_perms("server-csr.pem")
+print("Server Public Key:")
+print_pems.print_perms("server-public-key.pem")
+print("Signed Certificate:")
+print_pems.print_perms(signed_certificate)
 
 
 
